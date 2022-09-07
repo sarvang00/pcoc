43ca7f0221277174b15aaf76ea164bacf0fc9e8e
Sarvang Dave <sarvang00@gmail.om>
2022-09-07 16:13:19 -0300

    Commiting the updated build file to personal repo.

--- etc/build_docker.sh
+++ etc/build_docker.sh
@@ -3,9 +3,9 @@
 set -e
 
 IMAGE_NAME=pcoc
-TAG=v1.1.0
+TAG=v1.2.0
 DOCKERFILE_DIR=.
-REPO=carinerey/$IMAGE_NAME
+REPO=sarvang00/$IMAGE_NAME
 docker build -t $REPO:$TAG -f etc/Dockerfile $DOCKERFILE_DIR
 docker build -t $REPO -f etc/Dockerfile $DOCKERFILE_DIR
 if [[ $1 == "push_yes" ]]
